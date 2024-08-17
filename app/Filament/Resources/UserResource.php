a6f7f20d8f8c21b7b0d284479652a013a98ec3d0
jtfrancisco0121 <jtfrancisco21@gmail.com>
2024-08-17 14:14:30 +0800

    Order and User Relationship completed

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -16,6 +16,7 @@ use Filament\Forms\Components\DateTimePicker;
 use App\Filament\Resources\UserResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\UserResource\RelationManagers;
+use App\Filament\Resources\UserResource\RelationManagers\OrdersRelationManager;
 use Filament\Actions\DeleteAction;
 use Filament\Tables\Columns\TextColumn;
 
@@ -89,7 +90,7 @@ class UserResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            OrdersRelationManager::class
         ];
     }
 
